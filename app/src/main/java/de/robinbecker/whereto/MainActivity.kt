d2be04631119f797c29779aa77dfe7a628971464
Daria <daria.keim@web.de>
2020-05-23 22:24:54 +0200

    changes

--- app/src/main/java/de/robinbecker/whereto/MainActivity.kt
+++ app/src/main/java/de/robinbecker/whereto/MainActivity.kt
@@ -2,22 +2,57 @@ package de.robinbecker.whereto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-        toolbar?.title = "Toolbar"
-        toolbar?.navigationIcon = ContextCompat.getDrawable(this,R.drawable.ic_menu)
-        toolbar?.setNavigationOnClickListener { Toast.makeText(applicationContext,"Navigation icon was clicked",
-            Toast.LENGTH_SHORT).show() }
 
+        drawerLayout=findViewById(R.id.drawer_layout)
+        navView= findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout,toolbar, 0,0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_search -> {
+                Toast.makeText(this, "Search clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_settings -> {
+                Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_chat -> {
+                Toast.makeText(this, "Chat clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_reservations -> {
+                Toast.makeText(this, "Reservations clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_rating -> {
+                Toast.makeText(this, "Rating clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
