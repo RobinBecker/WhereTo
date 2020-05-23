1c9227051d30fbc2f2b8121d42a5867b58b56ac6
Daria <daria.keim@web.de>
2020-05-23 21:06:04 +0200

    changes

--- app/src/main/java/de/robinbecker/whereto/MainActivity.kt
+++ app/src/main/java/de/robinbecker/whereto/MainActivity.kt
@@ -2,11 +2,22 @@ package de.robinbecker.whereto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar?.title = "Toolbar"
+        toolbar?.navigationIcon = ContextCompat.getDrawable(this,R.drawable.icon_menu)
+        toolbar?.setNavigationOnClickListener { Toast.makeText(applicationContext,"Navigation icon was clicked",
+            Toast.LENGTH_SHORT).show() }
+
     }
 }
